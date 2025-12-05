9138065a489fa9a76d00e8792634fd603091a456
WESLEY ANJOS <wesleyanjoszero@gmail.com>
2025-12-05 14:39:45 -0300

    Adicionando operacao matematica

---
+++ aula06/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+	int minhaIdade = 20, maeIdade = 40, paiIdade = 45, irmaoIdade = 15;
+	int idadeTotal;
+	idadeTotal = minhaIdade, maeIdade, paiIdade, irmaoIdade;
+	printf("Idade total %i\n", idadeTotal);
+	return 0;
+}
