eab8bfb9df59a44043bb41bbefb84819fa9eee0a
WESLEY ANJOS <wesleyanjoszero@gmail.com>
2025-12-06 10:56:46 -0300

    tipos de variaveis

---
+++ aula08/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+	int a = 10;
+	float b = 10.5;
+	char c = 'a';
+	
+	printf("int: %i \nfloat: %f \nchar: %c \n", a, b, c);
+	printf("float: %.1f", b);
+	return 0;
+}
