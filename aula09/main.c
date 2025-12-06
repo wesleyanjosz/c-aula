38d6af2e01621769308e9c82b637fde8de0090b5
WESLEY ANJOS <wesleyanjoszero@gmail.com>
2025-12-06 11:03:04 -0300

    variavel tipo float

---
+++ aula09/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+	int n1 = 5; 
+	float n2 = 2; 
+	float r = n1 / n2;
+	
+	printf("O valor de n1 %i e o valor de n2 %f resulta em %f\n", n1, n2, r);
+	return 0;
+}
